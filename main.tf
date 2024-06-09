e9f8a158349b024f801ebcf8d32113437e8d5105
Francisco Pereira <franciscopere987@gmail.com>
2024-06-09 12:52:15 -0300

    RUN: tofu fmt

--- main.tf
+++ main.tf
@@ -2,36 +2,36 @@ terraform {
   backend "azurerm" {}
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~>3.106.1"
     }
   }
 }
 
 provider "azurerm" {
-   features {}
+  features {}
 }
 
 variable "location" {
-  type = string
+  type    = string
   default = "East US"
 }
 
 variable "firebase_key" {
-  type = string
+  type     = string
   nullable = false
 }
 
 module "container_apps" {
-  source = "./containerApps"
-  rg_name = azurerm_resource_group.chefcito.name
-  rg_id = azurerm_resource_group.chefcito.id
-  location = var.location
+  source       = "./containerApps"
+  rg_name      = azurerm_resource_group.chefcito.name
+  rg_id        = azurerm_resource_group.chefcito.id
+  location     = var.location
   firebase_key = var.firebase_key
 }
 
 resource "azurerm_resource_group" "chefcito" {
-  name = "chefcito-rg"
-  location = var.location 
-} 
+  name     = "chefcito-rg"
+  location = var.location
+}
 
