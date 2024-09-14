4ee166e5f2ad55eadf22bd35678f42f98a1e268a
Francisco Pereira <franciscopere987@gmail.com>
2024-09-14 17:17:45 -0300

    FIX: Account name

--- containerApps/main.tf
+++ containerApps/main.tf
@@ -152,7 +152,7 @@ resource "azurerm_service_plan" "queue_function_plan" {
   location = var.location
   resource_group_name = var.rg_name
   os_type= "Linux"
-  sku_name = "SHARED"
+  sku_name = "B1"
   
 }
 
